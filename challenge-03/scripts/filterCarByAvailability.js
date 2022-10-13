3d03ef3ef5cdb01c6a61742d0f7e62b151e4a619
zemonkey <61571621+zemonkey@users.noreply.github.com>
2022-10-13 21:01:42 +0700

    Added my answer for Challenge 03

---
+++ challenge-03/scripts/filterCarByAvailability.js
@@ -0,0 +1,19 @@
+function filterCarByAvailability(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Tempat penampungan hasil
+  const result = [];
+
+  // Tulis code-mu disini
+  cars.forEach(myFunction);
+
+  function myFunction(car, _index, _array) {
+    if (car.available) {
+      result.push(car);
+    }
+  }
+  console.log(result);
+  // Rubah code ini dengan array hasil filter berdasarkan availablity
+  return result;
+}
---
+++ challenge-03/scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,28 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  let result = [...cars];
+  
+  // Tulis code-mu disini
+
+  function bubbleSort(arr){
+    var len = arr.length;
+    for (var i = len-1; i>=0; i--){
+      for(var j = 1; j<=i; j++){
+        if(arr[j-1].year>arr[j].year){
+            var temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+         }
+      }
+    }
+    return arr;
+ }
+
+  result = bubbleSort(result)
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
